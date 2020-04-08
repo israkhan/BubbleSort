2f7b27f6df34cd0d76c5f3e3322652e22702986e
Isra Khan <israkhan2@gmail.com>
2020-04-08 14:59:50 -0500

    swap completed - still having issues with call count

--- bubblesort.js
+++ bubblesort.js
@@ -1,21 +1,63 @@
 function bubbleSort(array) {
-  return swap(array)
-}
-const sampleArr = [2,60,24,13]
+  let i = 0;
+  let j = i + 1;
 
-function swap(array) {
-  let len = array.length;
-  for(let i = 0; i < len; ++i) {
-    for(let j = 0; j < len; ++j){
-      let left = array[i]
-      let right = array[j]
-      if( array[i] < array[j]) {
-        continue
-      } else if(array[i] > array[j]) {
-        array[i] = right
-        array[j] = left
-        swap(array)
+  swap(array, i, j);
+
+  return array;
+}
+
+// function swap(array) {
+//   let len = array.length;
+//   let returnArr = [];
+
+//   for (let i = 0; i < len; ++i) {
+//     for (let j = 1 + i; j < len; ++j) {
+//       let left = array[i];
+//       let right = array[j];
+
+//       if (left < right) {
+//         return array;
+//       } else if (array[i] < array[j]) {
+//         console.log("second if");
+//         // array[i] = right;
+//         // array[j] = left;
+
+//         array.splice(j, 1);
+//         array.splice(i, 0, right);
+//         return swap(array);
+//       }
+//     }
+//   }
+// }
+
+const sampleArr = [2, 60, 24, 13];
+
+function swap(array, i, j) {
+  if (array.length <= 1) {
+    return array;
+  }
+  let left = array[i];
+  let right = array[j];
+
+  if (left < right) {
+    return swap(array, i + 1, i + 2);
+  } else if (array[i] > array[j]) {
+    array.splice(j, 1);
+    array.splice(i, 0, right);
+    return swap(array, i + 1, i + 2);
+  } else if (!right) {
+    let sorted = true;
+    for (let k = 0; k < array.length - 1; k++) {
+      if (array[k] > array[k + 1]) {
+        sorted = false;
       }
+    }
+
+    if (sorted === false) {
+      return swap(array, 0, 1);
+    } else {
+      return array;
     }
   }
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,29 +1,48 @@
 /* global bubbleSort */
 /* global swap */
 
-const sortingFunc = { swap, bubbleSort }
+const sortingFunc = { bubbleSort, swap };
 
-describe('Bubble Sort', function(){
+describe("Bubble Sort", function () {
+  beforeEach(function () {
+    const sortingFunc = {
+      swap,
+      bubbleSort,
+    };
 
-  beforeAll(function () {
-    spyOn(sortingFunc, 'swap').and.callThrough();
+    spyOn(sortingFunc, "swap").and.callThrough();
   });
 
-  it('handles an empty array', function(){
-    expect( sortingFunc.bubbleSort([]) ).toEqual( [] );
+  // beforeAll(function () {
+  //   spyOn(sortingFunc, "swap");
+  // });
+
+  it("handles an empty array", function () {
+    expect(sortingFunc.bubbleSort([])).toEqual([]);
   });
 
-  it('handles an array with 1 item', function(){
-    expect( sortingFunc.bubbleSort([5]) ).toEqual( [5] );
+  it("handles an array with 1 item", function () {
+    const array = [5];
+    expect(sortingFunc.bubbleSort(array)).toEqual([5]);
   });
 
-  it('handles an array with multiple items', function(){
-    expect( sortingFunc.bubbleSort([5,2,3,6,4,9,1]) ).toEqual( [1,2,3,4,5,6,9] );
+  it("handles an array with multiple items", function () {
+    expect(sortingFunc.bubbleSort([5, 2, 3, 6, 4, 9, 1])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      9,
+    ]);
   });
 
-  it('return the number of swaps required to sort an array', function () {
-    sortingFunc.bubbleSort([2,60,24,13]);
+  it("return the number of swaps required to sort an array", function () {
+    sortingFunc.bubbleSort([2, 60, 24, 13]);
+    expect(sortingFunc.swap.calls.any()).toEqual(true);
     expect(sortingFunc.swap.calls.count()).toEqual(7);
-  });
 
+    //expect(sortingFunc.swap.calls.count()).toEqual(7);
+  });
 });
