5053f07648f3e3c362b94d433aa74f9703037af1
Isra Khan <israkhan2@gmail.com>
2020-04-08 17:03:29 -0500

    merge completed, mergeSort in progress

--- mergesort.js
+++ mergesort.js
@@ -1,41 +1,68 @@
-
-const testArr = [9,2,8,1,5,0]
+const testArr = [9, 2, 8, 1, 5, 0];
 
 function split(wholeArray) {
-  if(wholeArray.length <= 1) {
-    return [wholeArray]
+  if (wholeArray.length <= 1) {
+    return [wholeArray];
   }
-  let middle = Math.floor(wholeArray.length / 2)
-  let firstHalf = wholeArray.slice(0, middle)
-  let secondHalf = wholeArray.slice(middle)
+  let middle = Math.floor(wholeArray.length / 2);
+  let firstHalf = wholeArray.slice(0, middle);
+  let secondHalf = wholeArray.slice(middle);
   /* your code here to define the firstHalf and secondHalf arrays */
 
   return [firstHalf, secondHalf];
 }
 
 function merge(arr1, arr2) {
-  if(arr2 === undefined) {
+  if (arr2 === undefined) {
     return arr1;
   }
-  if(arr1.length === 1 && arr2.length === 1) {
-    if(arr1[0] < arr2[0]) {
-      return [arr1[0], arr2[0]]
+  if (arr1.length === 1 && arr2.length === 1) {
+    if (arr1[0] < arr2[0]) {
+      return [arr1[0], arr2[0]];
     } else {
-      return [arr2[0], arr1[0]]
+      return [arr2[0], arr1[0]];
     }
   }
-  let tmpArr = []
+  let tmpArr = [];
   let i = 0;
   let j = 0;
-  while( i < arr1.length && j < arr2.length) {
-    if(arr1[i] < arr2[j]) {
-      tmpArr.push(arr1[i])
+  while (i <= arr1.length && j <= arr2.length) {
+    if (arr1[i] >= arr2[j] || (i === arr1.length && j !== arr2.length)) {
+      tmpArr.push(arr2[j]);
+      j++;
+    } else if (arr1[i] < arr2[j] || (i !== arr1.length && j === arr2.length)) {
+      tmpArr.push(arr1[i]);
       i++;
     } else {
-      tmpArr.push(arr2[j])
-      j++;
+      return tmpArr;
+    }
+  }
+}
+
+/*
+[12, 2, 6, 32, 13, 9, 15]
+[[12, 2, 6], [32, 13, 9, 15]]
+[12], [2, 6], [32, 13], [9, 15]
+[12], [2], [6], [32], [13], [9], [15]
+*/
+
+function mergeSort(array) {
+  /* your code here */
+  if (array.length <= 1) {
+    let newArr = split(array);
+    return merge(newArr[0]);
+  }
+
+  let stopSplit = true;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length !== 1) {
+      console.log("before ", array);
+      array = split(array);
+      console.log("after split ", array);
+      i = -1;
     }
   }
 
-  return tmpArr;
+  return array;
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,10 +1,10 @@
 /* global split */
 /* global merge */
+/* global mergeSort */
 
-const mergeFunc = { split, merge };
+const mergeFunc = { split, merge, mergeSort };
 
-
-describe('Split Array function', function() {
+describe("Split Array function", function () {
   it("handles an empty array", function () {
     expect(mergeFunc.split([])).toEqual([[]]);
   });
@@ -14,17 +14,54 @@ describe('Split Array function', function() {
     expect(mergeFunc.split(array)).toEqual([[5]]);
   });
 
-  it('is able to split an array into two halves', function() {
-    expect(mergeFunc.split([7,3,4,1,6])).toEqual([[7, 3],[4, 1, 6]]);
-    expect(mergeFunc.split([9,2,8,1,5,0])).toEqual([[9, 2, 8],[1, 5, 0]]);
+  it("is able to split an array into two halves", function () {
+    expect(mergeFunc.split([7, 3, 4, 1, 6])).toEqual([
+      [7, 3],
+      [4, 1, 6],
+    ]);
+    expect(mergeFunc.split([9, 2, 8, 1, 5, 0])).toEqual([
+      [9, 2, 8],
+      [1, 5, 0],
+    ]);
   });
 });
 
-describe('Merge function', function(){
-  it('is able to merge two sorted arrays into one sorted array', function(){
+describe("Merge function", function () {
+  it("is able to merge two sorted arrays into one sorted array", function () {
     // test the merging algorithm
-    expect(mergeFunc.merge([1, 3],[4, 6, 7])).toEqual([[1, 3, 4, 6, 7]])
-    expect(mergeFunc.merge([0, 1, 2],[5, 8, 9])).toEqual([[0, 1, 2, 5, 8, 9]])
-    expect(mergeFunc.merge([3, 5, 6],[2, 8, 9])).toEqual([[2, 3, 5, 6, 8, 9]])
+    expect(mergeFunc.merge([1, 3], [4, 6, 7])).toEqual([1, 3, 4, 6, 7]);
+    expect(mergeFunc.merge([0, 1, 2], [5, 8, 9])).toEqual([0, 1, 2, 5, 8, 9]);
+    expect(mergeFunc.merge([3, 5, 6], [2, 8, 9])).toEqual([2, 3, 5, 6, 8, 9]);
+  });
+});
+
+describe("mergeSort function", function () {
+  it("sorts array of length 0", function () {
+    expect(mergeFunc.mergeSort([])).toEqual([]);
+  });
+  it("sorts array of length 1", function () {
+    expect(mergeFunc.mergeSort([12])).toEqual([12]);
+  });
+  it("is able to sort an odd length array", function () {
+    expect(mergeFunc.mergeSort([12, 2, 6, 32, 13, 9, 15])).toEqual([
+      2,
+      6,
+      9,
+      12,
+      13,
+      15,
+      32,
+    ]);
+  });
+
+  it("is able to sort an even length array", function () {
+    expect(mergeFunc.mergeSort([12, 2, 6, 32, 13, 9])).toEqual([
+      2,
+      6,
+      9,
+      12,
+      13,
+      32,
+    ]);
   });
 });
